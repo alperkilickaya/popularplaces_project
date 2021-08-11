26d97e4aa89926b02db3918af7129323a8e939d8
Alper KILIÇKAYA <alperkilickaya@gmail.com>
2021-08-11 17:19:31 +0300

    added for loop to avoid multiple div enrty

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Header from './components/header/Header';
 import Cards from './components/cards/Card';
+import {data} from './data';
+
+const list = []
+
+  for(let i=0; i<data.length;i++) {
+    list.push(<Cards title={data[i].title} image={data[i].image} desc={data[i].desc} />)
+  }
 
 
 const App = () => {
@@ -10,7 +17,9 @@ const App = () => {
     <div>
       <Navbar />
       <Header />
-      <Cards />
+      <div className="container">
+        {list}
+      </div>
     </div>
   );
 }
--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -3,72 +3,17 @@ import {data} from '../../data'
 
 
 
-const Cards = () => {
+const Cards = (props) => {
     return (
-        <>
-            <div className ="container">
-                <div className="card">
-                    <p>{data[0].title}</p>
-                    <img src={data[0].image}></img>
-                    <p className="desc">{data[0].desc}</p>
-                </div>
-                <div className="card">
-                    <p>{data[1].title}</p>
-                    <img src={data[1].image}></img>
-                    <p className="desc">{data[1].desc}</p>
-                </div>
-                <div className="card">
-                    <p>{data[2].title}</p>
-                    <img src={data[2].image}></img>
-                    <p className="desc">{data[2].desc}</p>
-                </div>
-                <div className="card">
-                    <p>{data[3].title}</p>
-                    <img src={data[3].image}></img>
-                    <p className="desc">{data[3].desc}</p>
-                </div>
-                <div className="card">
-                    <p>{data[4].title}</p>
-                    <img src={data[4].image}></img>
-                    <p className="desc">{data[4].desc}</p>
-                </div>
-                <div className="card">
-                    <p>{data[5].title}</p>
-                    <img src={data[5].image}></img>
-                    <p className="desc">{data[5].desc}</p>
-                </div>
-                <div className="card">
-                    <p>{data[6].title}</p>
-                    <img src={data[6].image}></img>
-                    <p className="desc">{data[6].desc}</p>
-                </div>
-                <div className="card">
-                    <p>{data[7].title}</p>
-                    <img src={data[7].image}></img>
-                    <p className="desc">{data[7].desc}</p>
-                </div>
-                <div className="card">
-                    <p>{data[8].title}</p>
-                    <img src={data[8].image}></img>
-                    <p className="desc">{data[8].desc}</p>
-                </div>
-                <div className="card">
-                    <p>{data[0].title}</p>
-                    <img src={data[0].image}></img>
-                    <p className="desc">{data[0].desc}</p>
-                </div>
-                <div className="card">
-                    <p>{data[9].title}</p>
-                    <img src={data[9].image}></img>
-                    <p className="desc">{data[9].desc}</p>
-                </div>
-                <div className="card">
-                    <p>{data[10].title}</p>
-                    <img src={data[10].image}></img>
-                    <p className="desc">{data[10].desc}</p>
-                </div>
+        
+        
+            <div className="card">
+                <p>{props.title}</p>
+                <img src={props.image}></img>
+                <p className="desc">{props.desc}</p>
             </div>
-        </>
+    
+        
     )
 }
 
